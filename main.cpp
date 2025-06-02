52bf5729c13fff7811dbe6815a21aba394f1943a
juancruzrp <soycruzjuan@hotmail.com>
2025-06-01 23:58:30 -0300

    123

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main()
     MenuVenta menuventa;
     MenuInforme menuinforme;
 
-
+    /// holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     do{
     cout << "======================================" << endl;
     cout << "    SISTEMA DE GESTION FERRETERIA   " << endl ;
